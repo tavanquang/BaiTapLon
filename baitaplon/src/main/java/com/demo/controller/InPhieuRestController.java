550f185aa4636123cc096575c0a60164e45d3ac8
daomanh <daoducmanh28101997@mail.com>
2018-06-28 17:23:53 +0700

    ok
    ok

--- baitaplon/src/main/java/com/demo/controller/InPhieuRestController.java
+++ baitaplon/src/main/java/com/demo/controller/InPhieuRestController.java
@@ -3,13 +3,16 @@ package com.demo.controller;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -32,7 +35,8 @@ import com.itextpdf.text.DocumentException;
 @RequestMapping(value = "/api")
 public class InPhieuRestController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-
+	
+	public static final String pathFile = "Fileout/phieu.pdf";
 	@Autowired
 	FileOut fout;
 	@Autowired
@@ -47,31 +51,50 @@ public class InPhieuRestController {
 
 		LOGGER.info("da di vao ham");
 
-
 		TblPhieuDeXuatDTO deXuatDTO = phieuDeXuatService.getPhieuDeXuatDTO(id);
 		LOGGER.info(deXuatDTO.toString());
 
 		ClassLoader classLoader = getClass().getClassLoader();
 		File f = new File(classLoader.getResource("inphieudexuat/phieudexuat.html").getFile());
 		ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
-		ByteArrayInputStream inputstream =  phieuDeXuatService.inphieu(f, arrayOutputStream, deXuatDTO);
+		ByteArrayInputStream inputstream = phieuDeXuatService.inphieu(f, arrayOutputStream, deXuatDTO);
 
-		String filename = "phieudexuat.pdf";
-		InputStreamResource resource = new InputStreamResource(inputstream);
+		// String filename = "phieudexuat.pdf";
+		// InputStreamResource resource = new InputStreamResource(inputstream);
+		//
+
+		// HttpHeaders headers = new HttpHeaders();
+		// headers.add("content-disposition", "inline;filename=" + filename);
+		// headers.setContentType(MediaType.parseMediaType("application/pdf"));
+		//
+		// headers.setContentDispositionFormData(filename, filename);
+		// headers.setCacheControl("must-revalidate, post-check=0,
+		// pre-check=0");
+		//
+		// ResponseEntity<InputStreamResource> response = new
+		// ResponseEntity<>(resource, headers, HttpStatus.OK);
+		//
+
+		String fileName = "phieudexuat.pdf";
+		ClassPathResource pdfFile = new ClassPathResource(pathFile);
+		FileOutputStream outputFileName = new FileOutputStream(pdfFile.getFile());
+		IOUtils.copy(inputstream, outputFileName);
+
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("content-disposition", "inline;filename=" + filename);
 		headers.setContentType(MediaType.parseMediaType("application/pdf"));
-		
-		headers.setContentDispositionFormData(filename, filename);
-		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+		headers.add("Content-Disposition", "filename=" + fileName);
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
 
-		ResponseEntity<InputStreamResource> response = new ResponseEntity<>(resource, headers, HttpStatus.OK);
-		
+		headers.setContentLength(pdfFile.contentLength());
+		ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(
+				new InputStreamResource(pdfFile.getInputStream()), headers, HttpStatus.OK);
+
 		return response;
-		
-//		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "phieudexuat.pdf")
-//				.contentType(MediaType.APPLICATION_PDF).contentLength(arrayOutputStream.size()).body(resource);
-
 	}
 
 	@GetMapping("/phieuchi")
@@ -79,7 +102,6 @@ public class InPhieuRestController {
 			throws IOException, DocumentException {
 
 		LOGGER.info("da di vao ham");
-
 
 		TblPhieuChiDTO deChiDTO = phieuChiService.getPhieuChiDTO(id);
 		LOGGER.info(deChiDTO.toString());
@@ -89,11 +111,26 @@ public class InPhieuRestController {
 		ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
 		ByteArrayInputStream inputstream = phieuChiService.inphieu(f, arrayOutputStream, deChiDTO);
 
-		InputStreamResource resource = new InputStreamResource(inputstream);
+		String fileName = "phieuchi.pdf";
+		ClassPathResource pdfFile = new ClassPathResource(pathFile);
+		FileOutputStream outputFileName = new FileOutputStream(pdfFile.getFile());
+		IOUtils.copy(inputstream, outputFileName);
 
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "chieuchi.pdf")
-				.contentType(MediaType.APPLICATION_PDF).contentLength(arrayOutputStream.size()).body(resource);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.parseMediaType("application/pdf"));
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
+		headers.add("Access-Control-Allow-Headers", "Content-Type");
+		headers.add("Content-Disposition", "filename=" + fileName);
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
 
+		headers.setContentLength(pdfFile.contentLength());
+		ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(
+				new InputStreamResource(pdfFile.getInputStream()), headers, HttpStatus.OK);
+
+		return response;
 	}
 
 }
